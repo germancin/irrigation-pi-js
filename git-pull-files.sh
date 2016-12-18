62d7e6106641f5247d95aa394fc6cee19cefb247
German Gonzalez <german.gonzalez@zumba.com>
2016-12-18 08:19:09 -0500

    new files

--- git-pull-files.sh
+++ git-pull-files.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 git init 
+git config --global user.email "german.gonzalez@zumba.com"
+git config --global user.name "German Gonzalez"
 git remote add origin git@github.com:germancin/irrigation-pi-js.git
 git pull origin master
 git config branch.master.remote origin
