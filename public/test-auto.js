7d993011cda579a5ebb1a5b26945d9e56689f7d5
German Gonzalez <german.gonzalez@zumba.com>
2016-12-18 12:26:13 -0500

    new file

--- public/test-auto.js
+++ public/test-auto.js
@@ -1,11 +1,19 @@
-// test-auto.js
-// ========
+var gpio = require('rpi-gpio');
+var cron = require('cron').CronJob;
+var pin = 16;
+var delay = 5000;
+var timeZone = 'America/New_York';
+
 module.exports = {
-  foo: function () {
-    // whatever
-    console.log('o got into foo');
-  },
-  bar: function () {
-    // whatever
-  }
+
+	foo: function () {
+
+
+
+		console.log('Got  into foo' + timeZone);
+
+	},
+	bar: function () {
+		// whatever
+	}
 };
