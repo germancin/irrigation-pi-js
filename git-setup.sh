c352272df285c9959f76657a8c527e37553e2329
German Gonzalez <german.gonzalez@zumba.com>
2016-12-18 05:16:38 -0500

    setting up remote branch master

--- git-setup.sh
+++ git-setup.sh
@@ -3,3 +3,5 @@
 git init 
 git remote add origin git@github.com:germancin/irrigation-pi-js.git
 git pull origin master
+git config branch.master.remote origin
+git config branch.master.merge refs/heads/master
