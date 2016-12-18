1c543cc682c2a3538c43e01e761e7ca9146cf8e0
German Gonzalez <german.gonzalez@zumba.com>
2016-12-18 12:29:30 -0500

    new file

--- public/test-auto.js
+++ public/test-auto.js
@@ -13,7 +13,41 @@ module.exports = {
 		console.log('Got  into foo' + timeZone);
 
 	},
-	bar: function () {
-		// whatever
+	setCron: function () {
+
+		//new cron('00 06 07 * * 6', function() {
+
+			console.log('Start Watering Plant.')
+			wateringPlant();
+
+		//}, null, true, timeZone);
+
+	},
+	wateringPlant: function() {
+
+	    gpio.setup(pin, gpio.DIR_OUT, function(err) {
+
+	        gpio.write(pin, true, function(err) {
+	            if (err) throw err
+				gpio.read(pin, function(err, value) {
+					if(value == true){
+						turnOff()
+					}
+				})
+	        }) 
+	    })
 	}
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
