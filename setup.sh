23e4665ada47f88e503278d899c7364eda717637
German Gonzalez <german.gonzalez@zumba.com>
2016-12-18 04:53:27 -0500

    updating the server set up

--- setup.sh
+++ setup.sh
@@ -2,6 +2,15 @@
 
 clear
 echo "The script starts now."
-echo "Hi User, $USER"
+sudo apt-get update
+echo "Installing Linux appt: build-essential npm git tree vim raspi-config"
+sudo apt-get install -y build-essential npm git tree vim raspi-config
+echo "Downloading nodejs"
+wget http://node-arm.herokuapp.com/node_latest_armhf.deb
+sudo dpkg -i node_latest_armhf.deb
+sudo rm node_latest_armhf.deb
+sudo su
+sudo npm install -y rpi-gpio cron twilio express --save
+sudo npm install express-generator -g
 echo 
-echo "Now you can do more things..."
+echo "Done you are ready to go."
