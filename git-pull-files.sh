d525adf3b7db7b39093aec993f3dfcdbde71d405
German Gabriel <german.gonzalez@zumba.com>
2016-12-18 08:25:18 -0500

    Delete git-pull-files.sh

--- git-pull-files.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-git init 
-git config --global user.email "german.gonzalez@zumba.com"
-git config --global user.name "German Gonzalez"
-git remote add origin git@github.com:germancin/irrigation-pi-js.git
-git pull origin master
-git config branch.master.remote origin
-git config branch.master.merge refs/heads/master
