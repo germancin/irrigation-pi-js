c5566755328e16dee5337ab1ab7bd4241e89e61f
German Gonzalez <german.gonzalez@zumba.com>
2016-12-19 15:02:21 -0500

    updating code logic

--- public/modules/notification.js
+++ public/modules/notification.js
@@ -15,7 +15,7 @@ var self = module.exports = {
 		client.messages.create({
 		    body: 'Hola esto son buas notificaas',
 		    to: '+17543669331',  // Text this number
-		    from_: '+18134131741', // From a valid Twilio number
+		    from: '+18134131741', // From a valid Twilio number
 		    media_url: 'http://192.168.0.111/img/office-plant.jpg'
 		}, function(err, message) {
 
