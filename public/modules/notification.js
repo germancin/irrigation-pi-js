943140862c41f47725db29721d52f41691b0bb05
German Gonzalez <german.gonzalez@zumba.com>
2016-12-19 15:16:03 -0500

    updating code logic

--- public/modules/notification.js
+++ public/modules/notification.js
@@ -1,27 +1,34 @@
 "use strict"
 var express = require('express');
 var app = express();
-
+var twilio = require('twilio');
 var accountSid = 'AC4af2d1d9ea89fab22b21ff18a2348c99';
 var authToken = '7b1c09ae1c0c491f2b0dead446766d06';
 
 
 var self = module.exports = {
 
-	sendMMS: function (options, res) {
+	sendMMS: function (options) {
 
-		var twilio = require('twilio');
+		var client = new twilio.RestClient(accountSid, authToken);
 		var twiml = new twilio.TwimlResponse();
 
-		twiml.message(function() {
+		console.log(twiml);
+		// client.messages.create({
+		//     body: 'Hola esto son buas notificaas',
+		//     to: '+17543669331',  // Text this number
+		//     from: '+18134131741', // From a valid Twilio number
+		//     media_url: 'http://192.168.0.111/img/office-plant.jpg'
+		// }, function(err, message) {
 
-			  this.body('The Robots are coming! Head for the hills!');
-			  this.media('https://farm8.staticflickr.com/7090/6941316406_80b4d6d50e_z_d.jpg');
-
-	    });
-
-	    res.writeHead(200, {'Content-Type': 'text/xml'});
-	    res.end(twiml.toString());
+		//     if(err) {
+		//         console.error(err.message);
+		//         return false;
+		//     }
+		    
+		//     console.log('Message Sent: ' , message.sid);
+		//     return true;
+		// })
 
 	}
 };
