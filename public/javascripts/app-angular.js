2f1abfb049c0877aba309b693c88109a5e538a82
German Gonzalez <german.gonzalez@zumba.com>
2016-12-21 12:37:58 -0500

    updating code logic

--- public/javascripts/app-angular.js
+++ public/javascripts/app-angular.js
@@ -158,7 +158,7 @@ app.factory('ServiceStateDevice', ['$resource', function($resource) {
 }]);
 
 app.factory('rebootFactory', ['$resource', function($resource) {
-    return $resource("/reboot/:reboot", {action: '@reboot'}, {
+    return $resource("/reboot/:reboot", {reboot: '@reboot'}, {
         rebootDev: {
             method: 'GET'
         }
